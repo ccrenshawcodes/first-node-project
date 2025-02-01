00cbe0e00050432dcd336781e88c951456606555
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2025-01-31 17:07:31 -0700

    refactor to express

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-let http = require('http');
-let fs = require('fs');
-let url = require('node:url');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http.createServer(function(req, res) {
-  let path = url.parse(req.url, true);
+app.get("/", (req, res) => res.sendFile('/Users/catherine.crenshaw/repos/odin/my-projects/node-info-site/index.html'));
 
-  if (path.pathname === '/about') {
-    filepath = './about.html';
-  } else if (path.pathname === '/contact-me'){
-    filepath = './contact-me.html';
-  } else if (path.pathname === '/') {
-    filepath = './index.html';
-  } else {
-    filepath = './404.html';
-  }
-  
-  fs.readFile(filepath, function(err, data){
-    if (err) {
-      console.error(err);
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  })
-}).listen(8080);
+app.get("/about", (req, res) => res.sendFile('/Users/catherine.crenshaw/repos/odin/my-projects/node-info-site/about.html'));
+
+app.get("/contact-me", (req, res) => res.sendFile('/Users/catherine.crenshaw/repos/odin/my-projects/node-info-site/contact-me.html'))
+
+app.use((req, res) => res.sendFile('/Users/catherine.crenshaw/repos/odin/my-projects/node-info-site/404.html'))
+
+app.listen(port, () => {
+  console.log("it's working... it's working");
+})
